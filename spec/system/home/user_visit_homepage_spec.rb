3e2ac94f5afa28d52f022275b093225871b4f827
Luan <barkon32@tuta.io>
2023-06-06 18:04:11 -0300

    adicionada navbar e testada home page

--- spec/system/home/user_visit_homepage_spec.rb
+++ spec/system/home/user_visit_homepage_spec.rb
@@ -4,6 +4,7 @@ describe 'Visitante navega pela app' do
   it 'e vê tela inicial' do
     visit root_path
 
-    expect(page).to have_content 'Boas vindas'
+    expect(page).to have_content 'Loja do Clube'
+    expect(page).to have_link('Loja do Clube', href: root_path)
   end
 end
