d6cd93b73ec5ea6e31b3a1f32bf1fdf69d0c737b
Eduardo Celino <ehcelino@gmail.com>
2023-06-06 19:29:27 -0300

    Cadastro no sistema

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,18 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  enum role: { common: 0, admin: 1 }
+
+  before_create :define_role
+
+  private
+
+  def define_role
+    self.role = if email.present? && email.match(/\A[\w.+-]+@punti.com/)
+                  :admin
+                else
+                  :common
+                end
+  end
 end
---
+++ spec/system/user_creates_account_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+describe 'Usuário entra no sistema' do
+  it 'e cria uma conta' do
+    # Arrange
+
+    # Act
+    visit new_user_registration_path
+    fill_in 'Nome', with: 'João'
+    fill_in 'E-mail', with: 'joao@ig.com.br'
+    fill_in 'CPF', with: '26502001033'
+    fill_in 'Número de telefone', with: '19999999999'
+    fill_in 'Senha', with: 'password'
+    fill_in 'Confirme a senha', with: 'password'
+    click_on 'Registrar'
+
+    # Assert
+    expect(current_path).to eq root_path
+    expect(page).to have_content 'Bem vindo! Você se registrou com sucesso.'
+  end
+
+  it 'e cria uma conta administradora' do
+    visit new_user_registration_path
+    fill_in 'Nome', with: 'Aldaberto'
+    fill_in 'E-mail', with: 'aldaberto@punti.com'
+    fill_in 'CPF', with: '73962060065'
+    fill_in 'Número de telefone', with: '79981546487'
+    fill_in 'Senha', with: 'f4k3p455w0rd'
+    fill_in 'Confirme a senha', with: 'f4k3p455w0rd'
+    click_on 'Registrar'
+
+    # Assert
+    user = User.last
+
+    expect(current_path).to eq root_path
+    expect(user.role).to eq 'admin'
+    expect(page).to have_content 'administrador'
+  end
+end
