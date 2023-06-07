b0acf9c94a3c1eca2477910cdb0e4321f5a1a62c
TEREZA KANZAKI <tkanzaki@gmail.com>
2023-06-07 20:25:33 -0300

    Listagem de produto: Tela de listagem
    Co-authored-by: Matheus  matheusfersilva.10@gmail.com

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root "home#index"
-  resources :products, only: [:new, :create, :show]
+  resources :products, only: [:new, :create, :show, :index]
 end
---
+++ spec/system/user_view_products_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+describe 'Usuário acessa index de produtos' do
+  it 'e vê a lista de produtos' do
+    category = ProductCategory.create!(name: 'Eletrônico')
+    Product.create!(name: 'TV42', code: 'ABC123456',
+                    description: 'nova', brand: 'LG', price: 2500,
+                    product_category: category)
+    Product.create!(name: 'TV52', code: 'ABC654321',
+                    description: 'nova', brand: 'Samsung', price: 3500,
+                    product_category: category)
+    visit products_path
+
+    expect(page).to have_content 'Produtos'
+    expect(page).to have_content 'TV42'
+    expect(page).to have_content 'ABC123456'
+    expect(page).to have_content 'TV52'
+    expect(page).to have_content 'ABC654321'
+  end
+
+  it 'e não tem produtos cadastrados' do
+
+    visit products_path
+
+    expect(page).to have_content 'Produtos'
+    expect(page).not_to have_content 'TV42'
+    expect(page).to have_content 'Não existem produtos cadastrados.'
+  end
+
+  it 'e visualiza botão para cadastrar novo produto' do 
+    visit products_path
+
+    expect(page).to have_link 'Cadastrar Produto'
+  end
+end
