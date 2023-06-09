8880bc1636fa8fb3267a358d26c30d1fb323b62e
Luan <barkon32@tuta.io>
2023-06-09 18:19:00 -0300

    Adicionado teste de visitar área de cliente e view
    Co-authored-by: Matheus Barros <MatheusOB21@users.noreply.github.com>

--- spec/system/customer_area/user_visit_customer_area_spec.rb
+++ spec/system/customer_area/user_visit_customer_area_spec.rb
@@ -1,20 +1,37 @@
 require 'rails_helper'
 
 describe 'Usuário visita área do cliente' do
-  it 'e vê a área do cliente' do
+  it 'e não está logado' do
+    visit customer_area_index_path
+
+    expect(current_path).to eq new_user_session_path
+    expect(page).to have_content('Você precisa fazer login ou se registrar antes de continuar')
+  end
+
+  it 'a partir de um link na barra de navegação' do
     user = FactoryBot.create(:user, name: 'José', email: 'jose@gmail.com', password: 'jose1234')
+
+    login_as(user)
+    visit root_path
+
+    expect(page).to have_content 'Área do Cliente'
+  end
+
+  it 'e vê a área do cliente' do
+    user = FactoryBot.create(:user, name: 'José', email: 'jose@gmail.com', password: 'jose1234', cpf: '60789974088',
+                                    phone_number: '99923132')
 
     login_as(user)
     visit root_path
     click_on 'Área do Cliente'
 
     expect(current_path).to eq customer_area_index_path
-  end
-
-  it 'e não está logado' do
-    visit customer_area_index_path
-
-    expect(current_path).to eq new_user_session_path
-    expect(page).to have_content('Você precisa fazer login ou se registrar antes de continuar')
+    expect(page).to have_content 'José Cicrano Fulano'
+    expect(page).to have_content 'jose@gmail.com'
+    expect(page).to have_content '(11) 99992-3132'
+    expect(page).to have_link 'Endereços Cadastrados'
+    expect(page).to have_link 'Meus Pedidos'
+    expect(page).to have_link 'Produtos Favoritos'
+    expect(page).to have_link 'Minha Conta'
   end
 end
