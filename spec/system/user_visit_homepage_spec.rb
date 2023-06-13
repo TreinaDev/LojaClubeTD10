011c0499d45f34071b38c27c84a15cdfb53a5fe2
MatheusOB21 <matheus53barros@hotmail.com>
2023-06-12 21:07:53 -0400

    Adicionado imagem do produto na home page
    Co-authored-by: Eduardo Celino <ehcelino@gmail.com>

--- spec/system/user_visit_homepage_spec.rb
+++ spec/system/user_visit_homepage_spec.rb
@@ -15,11 +15,20 @@ describe 'Usuário visita homepage' do
     expect(page).to have_css 'nav'
   end
   it 'e vê os produtos recém adicionados' do
-    category = FactoryBot.create(:product_category, name: 'Camisetas')
-    FactoryBot.create(:product, name: 'Camiseta Azul', price: 800, product_category: category,
+    category_1 = FactoryBot.create(:product_category, name: 'Camisetas')
+    category_2 = FactoryBot.create(:product_category, name: 'Celulares')
+    FactoryBot.create(:product, name: 'Camiseta Azul', price: 800, product_category: category_1,
                                 description: 'Uma camisa azul muito bonita', code: 'CMA123456')
-    FactoryBot.create(:product, name: 'Camiseta Vermelha', price: 701, product_category: category,
+    FactoryBot.create(:product, name: 'Camiseta Vermelha', price: 701, product_category: category_1,
                                 description: 'Uma camisa vermelha bem grande', code: 'CMV123789')
+    product = Product.new(name: 'TV42',
+                          code: 'ABC123456',
+                          description: 'Descrição para o produto',
+                          brand: 'LG', price: 2500,
+                          product_category: category_2)
+    product.product_images.attach(io: Rails.root.join('spec/support/imgs/tv_2.jpg').open, filename: 'tv_2.jpg')
+    product.product_images.attach(io: Rails.root.join('spec/support/imgs/tv_22.jpg').open, filename: 'tv_22.jpg')
+    product.save!
 
     visit root_path
 
@@ -34,6 +43,13 @@ describe 'Usuário visita homepage' do
         expect(page).to have_content 'Camiseta Vermelha'
         expect(page).to have_content '701 Pontos'
         expect(page).to have_content 'Uma camisa vermelha bem grande'
+      end
+      within('.card#ABC123456') do
+        expect(page).to have_content 'TV42'
+        expect(page).to have_content '2500'
+        expect(page).to have_content 'Descrição para o produto'
+        expect(page).to have_content 'Descrição para o produto'
+        expect(page).to have_css('img[src*="tv_2.jpg"]')
       end
     end
   end
