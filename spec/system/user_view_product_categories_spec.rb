7ba2cdf41dfb3eee19b557496c6adef57400152c
Ricardonovais1 <ricardonovais1@gmail.com>
2023-06-14 08:24:59 -0300

    Realocação de um teste onde usuário comum não tem permissão de visualizar categorias de produtos, a não ser pelo menu
    Co-authored-by: Matheus  <matheusfersilva.10@gmail.com>

--- spec/system/user_view_product_categories_spec.rb
+++ spec/system/user_view_product_categories_spec.rb
@@ -1,15 +1,29 @@
 require 'rails_helper'
 
 describe 'Usuário vê as categorias dos produtos' do
-  it 'a partir da barra de navegação' do
-    FactoryBot.create(:product_category, name: 'Camisetas')
-    FactoryBot.create(:product_category, name: 'Eletrodomésticos')
-    FactoryBot.create(:product_category, name: 'Utensílios')
+  context 'com sucesso' do
+    it 'a partir da barra de navegação' do
+      FactoryBot.create(:product_category, name: 'Camisetas')
+      FactoryBot.create(:product_category, name: 'Eletrodomésticos')
+      FactoryBot.create(:product_category, name: 'Utensílios')
 
-    visit root_path
+      visit root_path
 
-    expect(page).to have_link 'Camisetas'
-    expect(page).to have_link 'Eletrodomésticos'
-    expect(page).to have_link 'Utensílios'
+      expect(page).to have_link 'Camisetas'
+      expect(page).to have_link 'Eletrodomésticos'
+      expect(page).to have_link 'Utensílios'
+    end
+  end
+
+  context 'pela página de categorias' do
+    it 'e não consegue acessar, pois não tem permissão' do
+      user = FactoryBot.create(:user)
+
+      login_as(user)
+      visit product_categories_path
+
+      expect(current_path).to eq root_path
+      expect(page).to have_content 'Você não possui permissão para realizar esta ação.'
+    end
   end
 end
