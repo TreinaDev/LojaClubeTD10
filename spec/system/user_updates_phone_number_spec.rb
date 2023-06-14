1d961aa89a7e3b5b80383ae19cddd6ef646b965e
Eduardo Celino <ehcelino@gmail.com>
2023-06-14 17:28:02 -0300

    Usuário atualiza o numero de telefone com sucesso
    Co-authored-by MatheusOB21 <matheus53barros@hotmail.com>

--- spec/system/user_updates_phone_number_spec.rb
+++ spec/system/user_updates_phone_number_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe 'Usuário atualiza o número de telefone' do
-  it 'a partir de do campo em minhas informações' do
+  it 'a partir do campo em minhas informações' do
     user = FactoryBot.create(:user, phone_number: '19934567890')
 
     login_as(user)
@@ -11,5 +11,19 @@ describe 'Usuário atualiza o número de telefone' do
 
     expect(page).to have_field 'phone_number', with: '(19)93456-7890'
     expect(page).to have_button 'Atualizar Contato'
+  end
+  it 'com sucesso' do
+    user = FactoryBot.create(:user, phone_number: '19934567890')
+
+    login_as(user)
+    visit root_path
+    click_on 'Área do Cliente'
+    click_on 'Minhas Informações'
+    fill_in 'phone_number',	with: '(19)93456-7823'
+    click_on 'Atualizar Contato'
+
+    expect(current_path).to eq me_path
+    expect(page).to have_content 'Número de telefone atualizado com sucesso'
+    expect(page).to have_field 'phone_number', with: '(19)93456-7823'
   end
 end
