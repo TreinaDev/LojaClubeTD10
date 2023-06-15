036896ace580640c256fe8009870c6c1a7b01da2
Eduardo Celino <ehcelino@gmail.com>
2023-06-15 19:31:34 -0300

    Corrigido erros do rubocop
    Co-authored-by: MatheusOB21 <matheus53barros@hotmail.com>

--- app/controllers/shopping_carts_controller.rb
+++ app/controllers/shopping_carts_controller.rb
@@ -1,30 +1,25 @@
 class ShoppingCartsController < ApplicationController
+  before_action :set_shopping_cart, only: %i[add remove]
+  before_action :params_product, only: %i[add]
+
   def show
     @shopping_cart = ShoppingCart.find(session[:cart_id])
   end
 
   def add
-    @shopping_cart ||= ShoppingCart.find_by(id: session[:cart_id])
-    if @shopping_cart.nil?
-      @shopping_cart = ShoppingCart.create
-      session[:cart_id] = @shopping_cart.id
-    end
-    @product = Product.find(params[:id])
     quantity = params[:quantity].to_i
     current_orderable = @shopping_cart.orderables.find_by(product_id: @product.id)
-    if current_orderable && quantity > 0
-      # quantity = current_orderable.quantity + quantity
-      current_orderable.update(quantity: quantity)
+    if current_orderable && quantity.positive?
+      current_orderable.update(quantity:)
     elsif quantity <= 0
       current_orderable.destroy
     else
-      @shopping_cart.orderables.create(product: @product, quantity: quantity)
+      @shopping_cart.orderables.create(product: @product, quantity:)
     end
     redirect_to @shopping_cart
   end
 
   def remove
-    @shopping_cart = ShoppingCart.find_by(id: session[:cart_id])
     Orderable.find_by(id: params[:id]).destroy
     if @shopping_cart.orderables.count <= 0
       @shopping_cart.destroy
@@ -32,5 +27,19 @@ class ShoppingCartsController < ApplicationController
       return redirect_to root_path
     end
     redirect_to @shopping_cart
+  end
+
+  private
+
+  def params_product
+    @product = Product.find(params[:id])
+  end
+
+  def set_shopping_cart
+    @shopping_cart = ShoppingCart.find_by(id: session[:cart_id])
+    return unless @shopping_cart.nil?
+
+    @shopping_cart = ShoppingCart.create
+    session[:cart_id] = @shopping_cart.id
   end
 end
--- app/helpers/shopping_carts_helper.rb
+++ app/helpers/shopping_carts_helper.rb
@@ -1,13 +1,6 @@
 module ShoppingCartsHelper
   def cart_total(cart)
-    if cart.nil?
-      return
-    else
-      if cart.total_items > 0
-        return cart.total_items
-      else
-        return 0
-      end
-    end
+    return if cart.nil?
+    return cart.total_items if cart.total_items.positive?
   end
 end
