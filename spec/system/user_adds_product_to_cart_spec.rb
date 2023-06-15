231179b09a6be11d4cb97f8950095d9cba8fa1de
Eduardo Celino <ehcelino@gmail.com>
2023-06-15 19:37:39 -0300

    Usuario adiciona produto ao carrinho e mais de um produto diferente, com sucesso
    Co-authored-by: MatheusOB21 <MatheusOB2matheus53barros@hotmail.com>

--- spec/system/user_adds_product_to_cart_spec.rb
+++ spec/system/user_adds_product_to_cart_spec.rb
@@ -45,4 +45,32 @@ describe 'Usuário adiciona produto ao carrinho' do
 
     expect(current_path).to eq root_path
   end
+  it 'e mais de um produto diferente, com sucesso' do
+    user = create(:user)
+    category1 = create(:product_category, name: 'Camisetas')
+    create(:product, name: 'Camiseta Azul', price: 800, product_category: category1,
+                     description: 'Uma camisa azul muito bonita', code: 'CMA123456')
+    create(:product, name: 'Camiseta Vermelha', price: 100, product_category: category1,
+                     description: 'Uma camisa vermelha muito grande', code: 'ZDS123789')
+    login_as(user)
+    visit root_path
+    click_on 'Camiseta Azul'
+    click_on 'Comprar'
+    click_on 'Continuar comprando'
+    click_on 'Camiseta Vermelha'
+    click_on 'Comprar'
+
+    expect(current_path).to eq shopping_cart_path(1)
+    expect(page).to have_content 'Carrinho de compras'
+    expect(page).to have_content 'Camiseta Azul'
+    expect(page).to have_content 'Valor em pontos: 800'
+    expect(page).to have_content 'Quantidade'
+    expect(page).to have_field 'quantity', with: '1'
+    expect(page).to have_content 'Camiseta Azul'
+    expect(page).to have_content 'Valor em pontos: 100'
+    expect(page).to have_content 'Quantidade'
+    expect(page).to have_field 'quantity', with: '1'
+    expect(page).to have_content 'Total:'
+    expect(page).to have_content '900 pontos'
+  end
 end
