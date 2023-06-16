c8f702efb723deb16df87e6bd9dc1647988bea37
Matheus Barros <92684440+MatheusOB21@users.noreply.github.com>
2023-06-16 14:10:08 -0400

    Revert "Categoria de produtos desativar"

--- spec/system/admin_disable_product_category_spec.rb
+++ spec/system/admin_disable_product_category_spec.rb
@@ -2,44 +2,44 @@ require 'rails_helper'
 
 describe 'Administrador desativa uma categoria' do
   it 'e vê mensagem de confirmação' do
-    cat = create(:product_category, name: 'Categoria Teste', active: true)
-    admin = create(:user, email: 'algumnome@punti.com')
-
-    login_as(admin)
-    visit root_path
-    within('nav') do
-      click_on 'Administração'
-      click_on 'Categorias'
-    end
-
-    find_button('Desativar', id: cat.id).click
-
-    expect(page).to have_content 'Você tem certeza? Isto irá afetar todos os produtos desta categoria.'
-  end
-
-  it 'com sucesso' do
-    cat = create(:product_category, name: 'Categoria Teste', active: true)
-    admin = create(:user, email: 'admin@punti.com')
+    FactoryBot.create(:product_category, name: 'Categoria Teste')
+    admin = FactoryBot.create(:user, email: 'admin@punti.com')
 
     login_as(admin)
     visit product_categories_path
-    find_button('Desativar', id: cat.id).click
-    cat.reload
+    click_on 'Editar'
+    click_on 'Desativar'
 
-    expect(cat.active).to be false
+    expect(page).to have_content 'Você tem certeza? Todos os produtos vinculados a essa categoria serão desativados.'
+  end
+
+  it 'com sucesso' do
+    category = FactoryBot.create(:product_category, name: 'Categoria Teste')
+    admin = FactoryBot.create(:user, email: 'admin@punti.com')
+
+    login_as(admin)
+    visit product_categories_path
+    click_on 'Editar'
+    click_on 'Desativar'
+    click_on 'Desativar categoria'
+    category.reload
+
+    expect(category.active).to be false
     expect(page).to have_content 'Categoria desabilitada com sucesso.'
   end
 
   it 'e reativa uma categoria com sucesso' do
-    cat = create(:product_category, name: 'Categoria Teste', active: false)
-    admin = create(:user, email: 'admin@punti.com')
+    category = FactoryBot.create(:product_category, name: 'Categoria Teste', active: false)
+    admin = FactoryBot.create(:user, email: 'admin@punti.com')
 
     login_as(admin)
     visit product_categories_path
-    find_button('Reativar', id: cat.id).click
-    cat.reload
+    click_on 'Editar'
+    click_on 'Reativar'
+    click_on 'Reativar categoria'
+    category.reload
 
-    expect(cat.active).to be true
+    expect(category.active).to be true
     expect(page).to have_content 'Categoria reativada com sucesso.'
   end
 end
