a66ae4000698bbbe4b50f7f93d91898914957c8f
Ricardonovais1 <ricardonovais1@gmail.com>
2023-06-16 22:01:52 -0300

    Ajustes referentes a factory bot e otimização de 3 testes de ativar e desativar categorias
    Co-authored-by: Matheus  <matheusfersilva.10@gmail.com>

--- spec/system/user_view_product_categories_spec.rb
+++ spec/system/user_view_product_categories_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 describe 'Usuário vê as categorias dos produtos' do
   it 'a partir da barra de navegação' do
-    FactoryBot.create(:product_category, name: 'Camisetas')
-    FactoryBot.create(:product_category, name: 'Eletrodomésticos')
-    FactoryBot.create(:product_category, name: 'Utensílios')
+    create(:product_category, name: 'Camisetas')
+    create(:product_category, name: 'Eletrodomésticos')
+    create(:product_category, name: 'Utensílios')
 
     visit root_path
 
