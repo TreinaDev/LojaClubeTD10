2e7330dfa2e7baba032b24fc8ef08cbcf0249ae3
Ricardonovais1 <ricardonovais1@gmail.com>
2023-06-19 12:31:14 -0300

    Testes de sucesso request favoritos

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,15 +2,18 @@ class FavoritesController < ApplicationController
   def create
     @favorite = Favorite.new(favorite_params)
     @product = Product.find(params[:favorite][:product_id])
-    return unless @favorite.save
-
+    @favorite.save
     redirect_to product_path(@product), notice: "#{@product.name} está na sua lista de produtos favoritos"
   end
 
   def destroy
     @favorite = Favorite.find params[:id]
-    if request.referer
-      redirect_to request.referer, notice: t('.destroy_success')
+    if @favorite.destroy
+      if request.referer
+        redirect_to request.referer, notice: t('.destroy_success')
+      else
+        redirect_to product_path(@favorite.product_id)
+      end
     else
       redirect_to request.referer, alert: t('.destroy_fails')
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_15_142259) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_19_145458) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -64,8 +64,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_15_142259) do
   end
 
   create_table "favorites", force: :cascade do |t|
-    t.integer "user_id", null: false
     t.integer "product_id", null: false
+    t.integer "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["product_id"], name: "index_favorites_on_product_id"
