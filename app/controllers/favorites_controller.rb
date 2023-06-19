535cf1259ac9dd421e3925c73b96cea8978f0371
Ricardonovais1 <ricardonovais1@gmail.com>
2023-06-19 17:56:06 -0300

    Testes de request para favoritos
    Co-authored-by: Matheus  <matheusfersilva.10@gmail.com>

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,32 +1,39 @@
 class FavoritesController < ApplicationController
-  before_action :authenticate_user!, only: %i[create destroy]
-  before_action :prevent_admin, only: %i[create destroy]
-  before_action :set_favorite, only: [:destroy]
+  before_action :prevent_admin, only: %i[create]
+  before_action :prevent_visitor, only: %i[create]
 
   def create
     @favorite = Favorite.new(favorite_params)
     @product = Product.find(params[:favorite][:product_id])
-    @favorite.save
-    redirect_to product_path(@product), notice: "#{@product.name} está na sua lista de produtos favoritos"
+    if @favorite.save
+      redirect_to product_path(@product), notice: "#{@product.name} está na sua lista de produtos favoritos"
+    else
+      redirect_to product_path(@product)
+    end
   end
 
   def destroy
+    @favorite = Favorite.find params[:id]
     if @favorite.destroy
+      redirect_definition
+    else
+      redirect_to request.referer, alert: t('.destroy_fails')
+    end
+  end
+
+  private
+
+  def redirect_definition
+    if request.referer
       redirect_to request.referer, notice: t('.destroy_success')
     else
       redirect_to product_path(@favorite.product_id)
     end
   end
 
-  private
-
   def favorite_params
     params
       .require(:favorite)
       .permit(:user_id, :product_id)
-  end
-
-  def set_favorite
-    @favorite = Favorite.find params[:id]
   end
 end
---
+++ spec/requests/user_sets_product_as_favorite_spec.rb
@@ -0,0 +1,59 @@
+require 'rails_helper'
+
+describe 'Usuário acessa rota de favorito de um produto' do
+  context 'enquanto cliente autenticado' do
+    it 'e marca produto como favorito com sucesso' do
+      user = create(:user, email: 'ricardo@exemple.com')
+      cat = create(:product_category, name: 'Categoria teste')
+      product = create(:product, product_category_id: cat.id)
+
+      login_as user
+      post favorites_path, params: { favorite: { user_id: user.id, product_id: product.id } }
+
+      expect(response).to have_http_status :found
+      expect(response).to redirect_to product_path(product)
+    end
+
+    it 'e desmarca um produto dos favoritos com sucesso' do
+      user = create(:user, email: 'ricardo@exemple.com')
+      cat = create(:product_category, name: 'Categoria teste')
+      product = create(:product, product_category_id: cat.id)
+
+      login_as user
+      post favorites_path, params: { favorite: { user_id: user.id, product_id: product.id } }
+      delete favorite_path(product)
+
+      expect(response).to have_http_status :found
+      expect(response).to redirect_to product_path(product)
+    end
+  end
+
+  context 'enquanto admin' do
+    it 'e tenta selecionar um favorito mas falha devido à falta de autorização' do
+      admin = create(:user, email: 'ricardo@punti.com')
+      cat = create(:product_category, name: 'Categoria teste')
+      product = create(:product, product_category_id: cat.id)
+
+      login_as admin
+      post favorites_path, params: { favorite: { user_id: admin.id, product_id: product.id } }
+
+      expect(response).to have_http_status 302
+      expect(flash[:alert]).to eq 'Administrador não tem acesso a essa página'
+      expect(response).to redirect_to root_path
+    end
+  end
+
+  context 'enquanto visitante' do
+    it 'e tenta selecionar um favorito mas falha devido à falta de autenticação' do
+      user = create(:user, email: 'ricardo@exemple.com')
+      cat = create(:product_category, name: 'Categoria teste')
+      product = create(:product, product_category_id: cat.id)
+
+      post favorites_path, params: { favorite: { user_id: user.id, product_id: product.id } }
+
+      expect(response).to have_http_status 302
+      expect(flash[:alert]).to eq 'Faça login para acessar'
+      expect(response).to redirect_to new_user_session_path
+    end
+  end
+end
