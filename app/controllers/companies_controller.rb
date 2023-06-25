9efec69b0ee513163bb16dbbaf8e53a818f787e2
TEREZA KANZAKI <tkanzaki@gmail.com>
2023-06-25 19:25:41 -0300

    Ajustes apontados nas reviews: testes de requests, sistemas e lógica da funcionalidade
    Co-authored-by: Ricardo Novais <ricardonovais1@gmail.com>

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -3,38 +3,40 @@ class CompaniesController < ApplicationController
   before_action :check_user, only: %i[index]
 
   def index
-    @companies_active = Company.where(active: true).order(:brand_name)
-    @companies_inactive = Company.where(active: false).order(:brand_name)
+    @companies = Company.order(:brand_name)
+    @companies_active = @companies.where(active: true)
+    @companies_inactive = @companies.where(active: false)
     begin
       response = Faraday.get('http://localhost:3000/api/v1/companies')
       response_tratament(response)
-      @companies_count_after = Company.count
-      flash.now[:alert] = t('.no_company_added') if @companies_count_before == @companies_count_after
-    rescue Faraday::ConnectionFailed
-      flash.now[:alert] = t('.api_error')
+    rescue StandardError
+      flash.now[:alert] = t('.internal_error')
     end
   end
 
-  private
-
   def response_tratament(response)
-    if response.status == 500
+    case response.status
+    when 200
+      iterates_companies(response)
+      flash_notice
+    when 204
+      flash.now[:notice] = t('.sucess_no_company')
+    else
       flash.now[:alert] = t('.internal_error')
-      render :index
     end
-    iterates_companies(response) if response.status == 200
   end
 
   def iterates_companies(response)
     @companies_count_before = Company.count
-    @companies_api = JSON.parse(response.body)
-    @companies_api.each do |cia|
+    companies_api = JSON.parse(response.body)
+    companies_api.each do |cia|
       company = Company.find_by(registration_number: cia['registration_number'])
 
       unless cia['active'].nil?
         company.nil? ? create(cia) : update(cia)
       end
     end
+    @companies_count_after = Company.count
   end
 
   def create(cia)
@@ -48,5 +50,13 @@ class CompaniesController < ApplicationController
     Company.find_by(registration_number: cia['registration_number']).update(brand_name: cia['brand_name'],
                                                                             corporate_name: cia['corporate_name'],
                                                                             active: cia['active'])
+  end
+
+  def flash_notice
+    flash.now[:notice] = if @companies_count_before == @companies_count_after
+                           t('.sucess_no_company_added')
+                         else
+                           t('.sucess_new_company_added')
+                         end
   end
 end
