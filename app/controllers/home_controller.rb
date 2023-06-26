833c7bc6cd6d1aa6e11db546a7cdf5a5dda84c6e
MatheusOB21 <matheus53barros@hotmail.com>
2023-06-26 13:37:58 -0400

    Usuário vê a campanha de acordo com a empresa que ele está associada
    Co-authored-by: Felipe Silva <ggfelipesilva@gmail.com>

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,17 @@
 class HomeController < ApplicationController
   def index
     @products = Product.order(created_at: :desc).filter { |p| p.product_category.active? }
+
+    @campaign_products = []
+    
+    cnpj = session[:company_cnpj]
+    company = Company.find_by(registration_number: cnpj)
+    if company&.promotional_campaigns.present?
+      company.promotional_campaigns.each do |campaign|
+        campaign.campaign_categories.each do |category|
+          @campaign_products << category.product_category.products
+        end
+      end
+    end
   end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -25,6 +25,7 @@ class SessionsController < Devise::SessionsController
   def response_company_treatment(response)
     data = JSON.parse(response.body)[0] if response.status == 200
     session[:status_user] = data.blank? ? 'visitor' : data['status']
+    session[:company_cnpj] = data['company_cnpj']
   end
 
   def response_card_treatment(response)
