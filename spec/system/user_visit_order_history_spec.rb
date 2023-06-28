a5c2a3a86e6a80282fb2c35fe1c5b1b14a8eefbe
felipel7 <felipelseven@gmail.com>
2023-06-27 21:13:10 -0300

    Adicionando testes para o histórico de pedido
    Co-authored-by: Matheus_ <MatFerreira@users.noreply.github.com>

--- spec/system/user_visit_order_history_spec.rb
+++ spec/system/user_visit_order_history_spec.rb
@@ -10,4 +10,48 @@ describe 'Usuário acessa histórico de pedidos' do
 
     expect(current_path).to eq order_history_path
   end
+
+  it 'com sucesso' do
+    user = create(:user)
+    order = create(:order, user:, status: 'pending', order_number: '32145672131')
+    order = create(:order, user:, status: 'approved', order_number: '12345678912')
+
+    login_as user
+    visit customer_areas_path
+    click_on 'Meus Pedidos'
+
+    expect(page).to have_content '32145672131'
+    expect(page).to have_content '12345678912'
+    expect(page).to have_content '20.000'
+    expect(page).to have_content 'pendente'
+    expect(page).to have_content 'aprovado'
+  end
+
+  it 'e recebe uma mensagem padrão quando não possui nenhum pedido' do
+    user = create(:user)
+
+    login_as user
+    visit customer_areas_path
+    click_on 'Meus Pedidos'
+
+    expect(page).to have_content 'Mensagem padrao'
+  end
+
+  it 'enquanto admin, mas não consegue e é redirecionado para a página inicial' do
+    admin = create(:user, email: 'admin@punti.com')
+
+    login_as(admin)
+    visit order_history_path
+  
+    expect(page).to have_content 'Administrador não tem acesso a essa página'
+    expect(current_path).to eq root_path
+  end
+
+  it 'enquanto visitante, mas não consegue e é redirecionado para o login' do
+    visit order_history_path
+
+    expect(page).to have_content 'Você precisa fazer login ou se registrar antes de continuar'
+    expect(current_path).to eq new_user_session_path
+  end
 end
+
