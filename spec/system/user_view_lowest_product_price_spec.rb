f0e4ef5609cce27a17743d83b89b124a2ffacff8
MatheusOB21 <matheus53barros@hotmail.com>
2023-06-28 09:07:18 -0400

    WIP: Usuário vê o menor preço do produto
    Co-authored-by: Ricardo Miranda de Novais <ricardonovais1@gmail.com>

---
+++ spec/system/user_view_lowest_product_price_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper' 
+
+describe 'Usuário vê o menor preço do produto' do
+  context 'no card' do
+    it 'existindo campanha com desconto maior que o preço sazonal, e sendo funcionário ativo' do
+      
+    end
+  end
+  
+
+end
