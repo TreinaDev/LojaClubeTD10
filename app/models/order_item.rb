d34cc7718de427ade1ae897f0bedcd8a81e4e6cc
clauf <82234131+ClaufSS@users.noreply.github.com>
2023-07-02 13:50:43 -0300

    Detalhes do pedido não pode ser modificado

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -2,5 +2,14 @@ class OrderItem < ApplicationRecord
   belongs_to :order
   belongs_to :product
 
-  validates :quantity, numericality: { greater_than: 0 }
+  validates :quantity, :price_amount, :discount_amount, numericality: { greater_than: 0 }
+  validates :price_amount, presence: true
+  validates :discount_amount, numericality: { less_than: :price_amount }
+  validate :block_changes, on: :update
+
+  private
+
+  def block_changes
+    errors.add(:base, 'Pedido não pode ser modificado')
+  end
 end
